7d6e846c0c853b56b20955538a76d5a4b9686d85
RajGGoswami <109051791+RajGGoswami@users.noreply.github.com>
2025-09-29 16:52:39 +1300

    Create tailwind.config.js

---
+++ optional-build/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["../index.html"],
+  theme: {
+    extend: {
+      colors: {
+        sitebgstart: '#071023',
+        sitebgend: '#0b1220',
+        accent: '#47abab',
+        accent2: '#34d399',
+      },
+      fontFamily: { sans: ['Inter', 'sans-serif'] }
+    }
+  },
+  plugins: []
+}
